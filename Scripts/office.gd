82621b227be58028f4256141a44d49c534a3ad4a
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-01-12 12:22:55 -0300

    Add logic for animate buttons and doors

--- Scripts/office.gd
+++ Scripts/office.gd
@@ -5,6 +5,11 @@ const CAM_SPEED: float = 10
 var isLeftSide: bool = false
 var isRightSide: bool = false
 
+var isLeftDoorOn: bool = false
+var isLeftLightOn: bool = false
+var isRightDoorOn: bool = false
+var isRightLightOn: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,7 +22,6 @@ func _process(delta):
 
 func _on_left_side_mouse_entered():
 	isLeftSide = true
-
 
 func _on_left_side_mouse_exited():
 	isLeftSide = false
@@ -34,3 +38,44 @@ func MoveCamera(isLeft: bool, isRight: bool) -> void:
 		$Camera.position.x -= CAM_SPEED
 	elif(isRight and $Camera.position.x < 780):
 		$Camera.position.x += CAM_SPEED
+
+func AnimateButtons(door: bool, light: bool, sprite: AnimatedSprite2D) -> void:
+	if(not door and not light):
+		sprite.play("stopped")
+		return
+	if(door and not light):
+		sprite.play("door")
+		return
+	if(not door and light):
+		sprite.play("light")
+		return
+	sprite.play("both")
+
+func AnimateDoors(doorOpen: bool, sprite: AnimatedSprite2D) -> void:
+	if(doorOpen):
+		sprite.play("close")
+		return
+	if(not doorOpen):
+		sprite.play_backwards("close")
+		return
+
+func _on_left_door_btn_pressed():
+	isLeftDoorOn = not isLeftDoorOn
+	AnimateButtons(isLeftDoorOn, isLeftLightOn, $Buttons/LeftButtons/LeftBtnSprite)
+	AnimateDoors(isLeftDoorOn, $Doors/LeftDoor)
+
+
+func _on_left_light_btn_pressed():
+	isLeftLightOn = not isLeftLightOn
+	AnimateButtons(isLeftDoorOn, isLeftLightOn, $Buttons/LeftButtons/LeftBtnSprite)
+
+
+func _on_right_door_btn_pressed():
+	isRightDoorOn = not isRightDoorOn
+	AnimateButtons(isRightDoorOn, isLeftLightOn, $Buttons/RightButtons/RightBtnSprite)
+	AnimateDoors(isRightDoorOn, $Doors/RigthDoor)
+
+
+func _on_right_light_btn_pressed():
+	isRightLightOn = not isRightLightOn
+	AnimateButtons(isRightDoorOn, isRightLightOn, $Buttons/RightButtons/RightBtnSprite)
