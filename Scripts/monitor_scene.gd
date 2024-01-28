8077b8f86822b1055af51747864dbbe0f6e6d485
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-01-28 18:50:12 -0300

    Add camera scene and assets

---
+++ Scripts/monitor_scene.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+# Decide the direction to move
+var moveToLeft : bool = false
+var cameraSpeed : int = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	MoveCameraAuto()
+
+# Move the camera auto.
+func MoveCameraAuto() -> void:
+	if $Camera.position.x < 473 or $Camera.position.x > 780:
+		cameraSpeed = -cameraSpeed
+	$Camera.position.x += cameraSpeed
