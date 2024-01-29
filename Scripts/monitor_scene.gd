f6806ed2ef9edc4976ac9396221ad1ae9ee25e30
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-01-29 02:29:36 -0300

    Group buttons

--- Scripts/monitor_scene.gd
+++ Scripts/monitor_scene.gd
@@ -4,6 +4,8 @@ extends Node2D
 var moveToLeft : bool = false
 var cameraSpeed : int = 1
 
+@export var btnGroup : ButtonGroup
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
