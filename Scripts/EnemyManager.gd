bbdff9097bff84027ed3a683b3c9e24846945912
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-02-12 21:19:23 -0300

    Add Script for every room to switch frame

---
+++ Scripts/EnemyManager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+class_name EnemyManager
+
+"""
+Night 1
+Bonnie and Chica attack, and Foxy if the player don't look 'Pirate Cove' Cam or is idle
+
+Bonnie 
+	-Path 1: Stage, Dining, West Hall, West Hall Corner, Left Door
+"""
---
+++ Scripts/RoomController.gd
@@ -0,0 +1,9 @@
+class_name RoomController
+
+extends AnimatedSprite2D
+
+func SwitchFrame(f: int) -> void:
+	frame = f
+
+func ResetFrame() -> void:
+	frame = 0
