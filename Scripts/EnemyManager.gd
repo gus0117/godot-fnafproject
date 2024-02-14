4bf23c71d76d7a8230770d745ab6e14ec80f9417
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-02-14 12:35:27 -0300

    Add EnemyController to manage the animatronics movement

--- Scripts/EnemyManager.gd
+++ Scripts/EnemyManager.gd
@@ -8,4 +8,44 @@ Bonnie and Chica attack, and Foxy if the player don't look 'Pirate Cove' Cam or 
 
 Bonnie 
 	-Path 1: Stage, Dining, West Hall, West Hall Corner, Left Door
+	cam1a, cam1b
 """
+
+var bonnieDict = {
+	"cam1a": [0,1,6],
+	"cam1b": [1,2],
+	"cam2a": [1,2,3],
+	"cam2b": [2],
+	"cam3" : [1],
+	"cam5" : [1,2]
+}
+var bCurrentPos = {
+	"camName" : "cam1a",
+	"frame" : 0
+}
+
+var chicaDict = {
+	"cam1a": "Show Stage",
+	"cam1b": "Dining Room",
+	"cam4a": "East Hall",
+	"cam4b": "E. Hall Corner",
+	"cam6" : "-Cam Disabled- \n*Audio Only*",
+	"cam7" : "Rest Room"
+}
+
+var freddyDict = {
+	"cam1a": "Show Stage",
+	"cam1b": "Dining Room",
+	"cam4a": "East Hall",
+	"cam4b": "E. Hall Corner",
+	"cam6" : "-Cam Disabled- \n*Audio Only*",
+	"cam7" : "Rest Room"
+}
+
+var foxyDict = {
+	"cam1c": "Pirate Cove",
+	"cam2a": "West Hall"
+}
+
+func MoveBonnie() -> void:
+	pass
